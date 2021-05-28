5d218612304f69e5d404d5ca66fbc9bec617b3bf
Konstantin Voykov <koykov0@gmail.com>
2021-05-28 23:22:39 +0300

    Simplify code.

--- bitset.go
+++ bitset.go
@@ -6,11 +6,11 @@ func (b *Bitset) SetBit(pos int, value bool) {
 	if pos < 0 || pos > 63 {
 		return
 	}
-	v := uint64(1 << pos)
+	v := Bitset(1 << pos)
 	if value {
-		*b = Bitset((uint64)(*b) | v)
+		*b = (*b) | v
 	} else {
-		*b = Bitset((uint64)(*b) & ^v)
+		*b = (*b) & ^v
 	}
 }
 
@@ -18,8 +18,8 @@ func (b *Bitset) CheckBit(pos int) bool {
 	if pos < 0 || pos > 63 {
 		return false
 	}
-	v := uint64(1 << pos)
-	return (uint64)(*b)&v != 0
+	v := Bitset(1 << pos)
+	return (*b)&v != 0
 }
 
 func (b *Bitset) Reset() {
